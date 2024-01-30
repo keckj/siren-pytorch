fd37aee239c345228923179357cff0d695e77606
Jean-Baptiste Keck <jbkeck@hotmail.com>
2024-01-31 00:21:22 +0100

    siren

--- tests/siren.py
+++ tests/siren.py
@@ -20,7 +20,7 @@ class TestActivation(unittest.TestCase):
     @classmethod
     def _test_activation(cls, fn):
         fn = fn.double().to(device)
-        x = torch.linspace(-np.pi, np.pi, 1024, dtype=torch.float64).to(device)
+        x = torch.linspace(-np.pi, np.pi, 64, dtype=torch.float64).to(device)
         eps = 1e-6
         for der in range(1,5):
             actual = fn(x, der=der)
@@ -43,8 +43,8 @@ class TestSiren(unittest.TestCase):
         def f(x, y):
             return siren(torch.column_stack([x, y]), max_derivative=max_derivative)
 
-        x = torch.rand(1024, dtype=torch.float64).to(device)
-        y = torch.rand(1024, dtype=torch.float64).to(device)
+        x = torch.rand(64, dtype=torch.float64).to(device)
+        y = torch.rand(64, dtype=torch.float64).to(device)
         eps = 1e-6
 
         z = f(x, y)
@@ -69,18 +69,18 @@ class TestSirenNet(unittest.TestCase):
         w0_initial = 2.0
 
         dim_in  = 2
-        dim_hidden = 32
+        dim_hidden = 5
         dim_out = 3
         num_layers = 4
-        max_derivative = 2
+        max_derivative = 3
 
         siren = SirenNet(dim_in, dim_hidden, dim_out, num_layers,
                 w0=w0, w0_initial=w0_initial, final_activation=Sigmoid()).double().to(device)
         def f(x, y):
             return siren(torch.column_stack([x, y]), max_derivative=max_derivative)
 
-        x = torch.rand(1024, dtype=torch.float64).to(device)
-        y = torch.rand(1024, dtype=torch.float64).to(device)
+        x = torch.rand(64, dtype=torch.float64).to(device)
+        y = torch.rand(64, dtype=torch.float64).to(device)
         eps = 1e-6
 
         z = f(x, y)
