fbff4606b527918f0a8d110fba34ecf1adb8669f
Jean-Baptiste Keck <jbkeck@hotmail.com>
2024-01-30 23:48:19 +0100

    siren

--- tests/siren.py
+++ tests/siren.py
@@ -45,7 +45,6 @@ class TestSiren(unittest.TestCase):
 
         x = torch.rand(1024, dtype=torch.float64).to(device)
         y = torch.rand(1024, dtype=torch.float64).to(device)
-
         eps = 1e-6
 
         z = f(x, y)
@@ -67,45 +66,38 @@ class TestSiren(unittest.TestCase):
 class TestSirenNet(unittest.TestCase):
     def test_derivative(self):
         w0 = 8.0
-        w0_initial = 30.0
+        w0_initial = 2.0
 
         dim_in  = 2
         dim_hidden = 32
         dim_out = 3
         num_layers = 4
+        max_derivative = 2
 
         siren = SirenNet(dim_in, dim_hidden, dim_out, num_layers,
                 w0=w0, w0_initial=w0_initial, final_activation=Sigmoid()).double().to(device)
         def f(x, y):
-            return siren(torch.column_stack([x, y]), max_derivative=2)
+            return siren(torch.column_stack([x, y]), max_derivative=max_derivative)
 
         x = torch.rand(1024, dtype=torch.float64).to(device)
         y = torch.rand(1024, dtype=torch.float64).to(device)
         eps = 1e-6
 
-        z, J, H = f(x, y)
-        assert z.shape == (1024, dim_out), z.shape
-        assert J.shape == (1024, dim_out, dim_in), J.shape
-        assert H.shape == (1024, dim_out, dim_in, dim_in), H.shape
+        z = f(x, y)
 
-        yp, Jp, Hp = f(x+eps, y)
-        yn, Jn, Hn = f(x-eps, y)
-        dz_dx = (yp-yn)/(2*eps)
-        Jz_dx = (Jp-Jn)/(2*eps)
+        zxp = f(x+eps, y)
+        zxn = f(x-eps, y)
+        zyp = f(x, y+eps)
+        zyn = f(x, y-eps)
 
-        yp, Jp, Hp = f(x, y+eps)
-        yn, Jn, Hn = f(x, y-eps)
-        dz_dy = (yp-yn)/(2*eps)
-        Jz_dy = (Jp-Jn)/(2*eps)
+        for i in range(1, max_derivative+1):
+            ev = z[i]
+            fd = torch.stack([zxp[i-1]-zxn[i-1], zyp[i-1]-zyn[i-1]], -1) / (2*eps)
 
-        Jfd = torch.stack([dz_dx, dz_dy], -1)
-        Hfd = torch.stack([Jz_dx, Jz_dy], -1)
+            max_err = abs(ev-fd).max()
+            print(f'SirenNet d{i}f={max_err:2.2e}')
+            assert torch.allclose(ev, fd, rtol=1e-8)
 
-        J_max_err = abs(J-Jfd).max()
-        H_max_err = abs(H-Hfd).max()
-        print(f'Siren Jerr={J_max_err:2.2e}, Herr={H_max_err:2.2e},')
-        assert torch.allclose(J, Jfd, rtol=1e-8)
-        assert torch.allclose(H, Hfd, rtol=1e-6)
 
 if __name__ == '__main__':
     unittest.main()
